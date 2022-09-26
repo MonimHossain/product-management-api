b6750c53479f2c86a06a61c1b1bfa3ef19fc5484
Monim Hossain <flynmonim@gmail.com>
2022-09-27 03:10:34 +0600

    product link create and backup image ready

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -29,4 +29,9 @@ class Product extends Model
     {
         return $this->belongsTo(ProductCategory::class, 'category_id', 'id');
     }
+
+    public function getImageUrlAttribute($value)
+    {
+        return json_decode($value, true);
+    }
 }
